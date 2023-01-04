c6b0752ea881e47d494a80dfe834047376a0910a
hrmojtahed <hrmojtahed@gmail.com>
2023-01-04 15:05:07 +0400

    create custom user model and manager

--- profiles_core/settings.py
+++ profiles_core/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+
+    'profiles_api'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
